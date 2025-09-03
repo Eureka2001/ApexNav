c1de4e49f5912502aee989465fe41c48697777d3
Yiyao Wang <yiyao.wang@vipl.ict.ac.cn>
2025-09-03 15:30:21 +0800

    fix: 修复 MobileSAM 的默认端口号问题

--- vlm/config.py
+++ vlm/config.py
@@ -3,5 +3,5 @@ import os
 # VLM服务端口环境变量配置
 VLM_GROUNDING_DINO_PORT = int(os.environ.get('VLM_GROUNDING_DINO_PORT', 12181))
 VLM_BLIP2ITM_PORT = int(os.environ.get('VLM_BLIP2ITM_PORT', 12182))
-VLM_MOBILE_SAM_PORT = int(os.environ.get('VLM_MOBILE_SAM_PORT', 12283))
+VLM_MOBILE_SAM_PORT = int(os.environ.get('VLM_MOBILE_SAM_PORT', 12183))
 VLM_YOLOV7_PORT = int(os.environ.get('VLM_YOLOV7_PORT', 12184))
